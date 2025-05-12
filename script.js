3dd4b284675169d1eaeed021fc9dcf0e5f4e821e
Babatunde Ishau <ishautunde700@gmail.com>
2025-05-12 23:35:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -118,6 +118,10 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-window.addEventListener("load", () => {
-  document.querySelector(".preloader").style.display = "none";
-});
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    preloader.style.opacity = '0';
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 500);
+  });
